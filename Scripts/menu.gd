806db8ad94e7ad2749ac05a465b6ed05f1513178
Sarahi Capi Casillas <s240349@th-ab.de>
2025-06-05 16:54:02 +0200

    Creating the cooking scene, where the player can add ingredients to a basket

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,4 +1,17 @@
 extends Node2D
 
+# Storing the labels in variables
+@onready var toast = $Ingredients/ToastLabel
+@onready var cheese = $Ingredients/CheeseLabel
+@onready var tomatoes = $Ingredients/TomatoesLabel
+@onready var lettuce = $Ingredients/LettuceLabel
+
+func _ready()  -> void:
+	# Displaying the numbers
+	toast.text = str(Global.toastNumber)
+	cheese.text = str(Global.cheeseNumber)
+	tomatoes.text = str(Global.tomatoesNumber)
+	lettuce.text = str(Global.lettuceNumber)
+	
 func _on_next_button_pressed() -> void:
-		get_tree().change_scene_to_file("res://Scenes/nomnom_eats.tscn")
+		get_tree().change_scene_to_file("res://Scenes/cooking.tscn")
